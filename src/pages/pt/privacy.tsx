fe0a8c3f5ca45567d46b4d952ac18632514ad9d9
Rafael Lima <contato@rafael.adm.br>
2021-11-27 11:56:54 -0300

    Translation of Privacy Policy

---
+++ src/pages/pt/privacy.tsx
@@ -0,0 +1,98 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import React from 'react';
+import Image from 'next/image';
+import Social from '../../components/IndexPage/Social';
+import Footer from '../../components/IndexPage/Footer';
+import Link from '../../components/NextLink';
+import ChooseLanguage from '../../components/ChooseLanguage';
+
+const Privacy: NextPage = () => {
+  return (
+    <div className="flex flex-col min-h-screen h-full bg-bgprimary">
+      <ChooseLanguage />
+      <Head>
+        <title>Cobogo - Política de Privacidade</title>
+      </Head>
+      <div className="mx-5 md:mx-20 py-20">
+        <Link href="/">
+          <div className="w-60 h-14 relative">
+            <Image
+              layout="fill"
+              objectFit="contain"
+              src="/logos/cobogo-logo.svg"
+              alt="Guy playing youtube site"
+            />
+          </div>
+        </Link>
+      </div>
+      <div className="flex flex-1 flex-col mx-5 md:mx-20 pb-20">
+        <h1 className="bold-36 mb-5">Política de Privacidade</h1>
+        <p className="regular-18 break-normal mb-5">
+          cobogo opera o site https://cobogo.social, que fornece o Serviço dApp.
+        </p>
+        <p className="regular-18 break-normal mb-5">
+          Esta página é usada para informar os visitantes do site sobre nossas políticas em relação à coleta, uso e divulgação de Informações Pessoais de qualquer pessoa que decida usar nosso Serviço, o dApp do cobogo social e o site.
+        </p>
+        <p className="regular-18 break-normal mb-5">
+          Ao usar nosso Serviço, você concorda com a coleta e uso de informações de acordo com esta política. As informações pessoais que coletamos são usadas para fornecer e melhorar o Serviço. Não usaremos ou compartilharemos suas informações com ninguém, exceto conforme descrito nesta Política de Privacidade.
+        </p>
+        <p className="regular-18 break-normal mb-5">
+          Os termos usados nesta Política de Privacidade têm os mesmos significados que em nossos Termos e Condições, que podem ser acessados em https://cobogo.social, a menos que definido de outra forma nesta Política de Privacidade.
+        </p>
+        <h2 className="bold-30 mb-2">Coleta e Uso de Informações</h2>
+        <p className="regular-18 break-normal mb-5">
+          Para uma melhor experiência ao usar nosso Serviço, podemos requerer que você nos forneça certas informações de identificação pessoal, incluindo, mas não se limitando a seu nome, número de telefone e endereço postal. As informações que coletamos serão usadas para contatá-lo ou identificá-lo.
+        </p>
+        <h2 className="bold-30 mb-2">Dados de Registro</h2>
+        <p className="regular-18 break-normal mb-5">
+          Queremos informar que sempre que você visita nosso Serviço, coletamos informações que o seu navegador nos envia, chamadas de “Dados de Registro”. Esses dados de registro podem incluir informações como o endereço de protocolo da Internet (&quot;IP&quot;) do seu computador, a versão do navegador, as páginas que você visitou em nosso serviço, a hora e a data da sua visita, o tempo gasto nessas páginas e outras estatísticas.
+        </p>
+        <h2 className="bold-30 mb-2">Cookies</h2>
+        <p className="regular-18 break-normal mb-5">
+          Cookies são arquivos com pequenas quantidades de dados que são comumente usados como um identificador anônimo exclusivo. Eles são enviados para o seu navegador a partir do site que você visita e são armazenados no disco rígido do seu computador.
+        </p>
+        <p className="regular-18 break-normal mb-5">
+          Nosso site usa esses cookies para coletar informações e melhorar nosso serviço. Você tem a opção de aceitar ou recusar esses cookies, e saber quando um cookie está sendo enviado para o seu computador. Se você optar por recusar nossos cookies, pode não ser capaz de usar algumas partes de nosso Serviço.
+        </p>
+        <h2 className="bold-30 mb-2">Provedores de Serviço</h2>
+        <p className="regular-18 break-normal mb-5">
+          Podemos empregar empresas terceirizadas e indivíduos pelos seguintes motivos:
+        </p>
+        <ul className="regular-16 list-disc pl-20 mb-3">
+          <li>Para facilitar nosso Serviço;</li>
+          <li>Para fornecer o Serviço por nós;</li>
+          <li>Para realizar serviços relacionados com o Serviço; ou</li>
+          <li>Para nos ajudar a analisar como nosso serviço está sendo usado.</li>
+        </ul>
+        <p className="regular-18 break-normal mb-5">
+          Queremos informar aos usuários do nosso Serviço que esses terceiros têm acesso às suas Informações Pessoais, para que possam realizar as tarefas atribuídas a eles por nós. No entanto, eles são obrigados a não divulgar ou usar as informações para qualquer outra finalidade.
+        </p>
+        <h2 className="bold-30 mb-2">Segurança</h2>
+        <p className="regular-18 break-normal mb-5">
+          Valorizamos sua confiança em nos fornecer suas informações pessoais, portanto, nos esforçamos para usar meios comercialmente aceitáveis de protegê-las. Note que nenhum método de transmissão pela Internet ou método de armazenamento eletrônico é 100% seguro e confiável, e não podemos garantir sua segurança absoluta.
+        </p>
+        <h2 className="bold-30 mb-2">Links para Outros Sites</h2>
+        <p className="regular-18 break-normal mb-5">
+          Nosso serviço pode conter links para outros sites. Se você clicar em um link de terceiros, será direcionado a esse site. Observe que esses sites externos não são operados por nós. Portanto, recomendamos enfaticamente que você analise a Política de Privacidade desses sites. Não temos controle e não assumimos responsabilidade pelo conteúdo, políticas de privacidade ou práticas de quaisquer sites ou serviços de terceiros.
+        </p>
+        <p className="bold-30 mb-2">Privacidade Infantil</p>
+        <p className="regular-18 break-normal mb-5">
+          Nossos Serviços não se dirigem a ninguém com idade inferior a 13 anos. Não coletamos intencionalmente informações pessoais identificáveis de crianças menores de 13 anos. No caso de descobrirmos que uma criança menor de 13 anos nos forneceu informações pessoais, nós as excluímos imediatamente de nossos servidores. Se você é um pai ou responsável e está ciente de que seu filho nos forneceu informações pessoais, entre em contato para que possamos tomar as medidas necessárias.
+        </p>
+        <h2 className="bold-30 mb-2">Mudanças nesta Política de Privacidade</h2>
+        <p className="regular-18 break-normal mb-5">
+          Podemos atualizar nossa Política de Privacidade de tempos em tempos. Assim, aconselhamos você a revisar esta página periodicamente para quaisquer alterações. Iremos notificá-lo de qualquer alteração, publicando a nova Política de Privacidade nesta página. Essas alterações entram em vigor imediatamente após serem publicadas nesta página.
+        </p>
+        <h2 className="bold-30 mb-2">Entre em Contato</h2>
+        <p className="regular-18 break-normal mb-5">
+          Se você tiver dúvidas ou sugestões sobre nossa Política de Privacidade, não hesite em nos contatar.
+        </p>
+      </div>
+      <Social />
+      <Footer />
+    </div>
+  );
+};
+
+export default Privacy;
