bf5403512e4eb5926134ba18e52c9d7d21556d17
Thiago Machado <thiagomachado@pop-os.localdomain>
2022-01-25 12:32:55 -0300

    ui fix

--- src/components/CallToActionButton.tsx
+++ src/components/CallToActionButton.tsx
@@ -13,19 +13,19 @@ export default function CallToActionButton({
     <>
       {color === 'purple' && (
         <button
-          className="bold-24 bg-purple hover:bg-purplehover h-[50px] py-0 px-5"
+          className="bold-24 bg-purple hover:bg-purplehover h-[50px] py-0 px-5 flex justify-center items-center"
           onClick={onClick}
         >
-          <span className="text-md md:text-xl lg:text-2xl">{placeholder}</span>
+          <span className="text-sm md:text-xl lg:text-2xl">{placeholder}</span>
         </button>
       )}
 
       {color === 'purple-light' && (
         <button
-          className="bold-24 bg-purplelight hover:bg-purplelighthover h-[50px] py-0 px-5"
+          className="bold-24 bg-purplelight hover:bg-purplelighthover h-[50px] py-0 px-5 flex justify-center items-center"
           onClick={onClick}
         >
-          <span className="text-md md:text-xl lg:text-2xl">{placeholder}</span>
+          <span className="text-sm md:text-xl lg:text-2xl">{placeholder}</span>
         </button>
       )}
     </>
