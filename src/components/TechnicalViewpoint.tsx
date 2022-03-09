3a3df741fa29d5662b7af745287ca665720c6aac
Thiago Machado <contatocostathiago@gmail.com>
2022-03-09 19:54:43 -0300

    update technical viewpoint graphic

--- src/components/TechnicalViewpoint.tsx
+++ src/components/TechnicalViewpoint.tsx
@@ -8,29 +8,28 @@ export default function TechnicalViewpoint() {
     <section className="flex flex-col items-center h-full w-full py-24 bg-secondary">
       <div className="flex flex-col items-center">
         <h1 className="bold-36 text-center pb-7">{t('technical viewpoint')}</h1>
+
         <h2 className="light-24 text-center pb-0 md:pb-20 w-screen md:w-1/2 md:px-0 px-5">
           {t(
             'cobogo is a dapp that leverages the Dev Protocol funding system to sustainably fund YouTube creators. Fans can stake CBG tokens on their favorite YouTuber pool and automatically start getting paid with CBG rewards.'
           )}
         </h2>
       </div>
-      <div className="image">
-        <div className="w-full h-full relative">
-          <Image
-            layout="fill"
-            objectFit="contain"
-            sizes="100%"
-            src="/images/technical-viewpoint.svg"
-            alt="technical viewpoint flow"
-          />
-        </div>
-      </div>
+
+      <Image
+        width={801}
+        height={443}
+        src="/images/technical-viewpoint.png"
+        alt="technical viewpoint"
+      />
+
       <div className="flex flex-col lg:flex-row w-full sm:justify-center lg:justify-between px-5 md:px-20">
         <div className="regular-18 text-left lg:w-1/3 pr-24 lg:pr-0 pb-20 lg:pb-0">
           {t(
             'with CBG (NFT), creators can design all the perks for their fans. In this way, everyone that uses this method of funding can receive benefits not found anywhere else.'
           )}
         </div>
+
         <div className="regular-18 text-right lg:w-1/3 pl-24 lg:pl-0">
           {t(
             'even small and niche YouTubers will be able to be properly monetized by their own community. Through cobogo, community building and monetization is taken to a new level not found in web2.'
