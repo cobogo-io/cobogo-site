1eb8c4e57c076ba0ff9eba32e3e7a1bbafb738e1
Thiago Machado <contatocostathiago@gmail.com>
2022-09-15 11:58:20 -0300

    feat: adds underline on h1 tag and creates Bullets component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { BackgroundOne } from '../src/components/BackgroundOne'
 import { Button } from '../src/components/Button'
+import { Bullets } from '../src/components/images/Bullets'
 import { TweetProfile } from '../src/components/images/TweetProfile'
 
 import type { NextPage } from 'next'
@@ -8,10 +9,11 @@ const Home: NextPage = () => {
   return (
     <div className="relative flex flex-col justify-center items-center h-screen">
       <div className="flex flex-col items-center gap-16 z-50">
-        <h1 className="text-7xl max-w-[1030px] text-center">
+        <h1 className="text-7xl max-w-[1030px] text-center relative">
           Get <strong>incorporated</strong>, <strong>funded</strong>,{' '}
           <strong>recognized</strong>, and <strong>skyrocket</strong> your{' '}
           <strong>Creator career</strong>
+          <div className="w-[470px] h-[19px] bg-white-100/20 absolute bottom-1 left-1/2 -translate-x-1/2"></div>
         </h1>
         <Button />
       </div>
@@ -23,6 +25,7 @@ const Home: NextPage = () => {
           <strong>- @MrBeast</strong>
         </p>
       </div>
+      <Bullets />
       <BackgroundOne />
     </div>
   )
---
+++ src/components/images/Bullets.tsx
@@ -0,0 +1,17 @@
+export const Bullets = (): JSX.Element => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="851"
+      height="504"
+      fill="none"
+      viewBox="0 0 851 504"
+      className="absolute z-10"
+    >
+      <circle cx="6" cy="85" r="6" fill="#00B9E9"></circle>
+      <circle cx="712" cy="6" r="6" fill="#BA68C8"></circle>
+      <circle cx="845" cy="384" r="6" fill="#6CB14B"></circle>
+      <circle cx="58" cy="498" r="6" fill="#5C8295"></circle>
+    </svg>
+  )
+}
