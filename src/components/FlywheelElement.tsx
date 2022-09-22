00e6f27abb3bf2bf2d9bd0848218d1595bca925b
Thiago Machado <contatocostathiago@gmail.com>
2022-09-22 12:43:43 -0300

    feat: creates elements on Flywheel fold component

--- src/components/FlywheelElement.tsx
+++ src/components/FlywheelElement.tsx
@@ -1,14 +1,20 @@
+import { Dispatch, SetStateAction } from 'react'
+
+type Element = null | 'Social' | 'Start' | 'Fundraise' | 'Ventures'
+
 interface FlywheelElementProps {
-  text: string
-  color: 'blue' | 'green' | 'gray' | 'pink'
+  text: Element
+  color: string
   inverted?: boolean
   inRow?: boolean
+  elementOpened: Element
+  setElementOpened: Dispatch<SetStateAction<Element>>
 }
 
 export const FlywheelElement = (props: FlywheelElementProps): JSX.Element => {
   return (
     <div
-      className={`flex items-center relative gap-3 hover:cursor-pointer ${
+      className={`flex items-center relative gap-3 hover:cursor-pointer transition-all ${
         props.inverted && props.inRow
           ? 'flex-row-reverse'
           : props.inverted
@@ -17,25 +23,46 @@ export const FlywheelElement = (props: FlywheelElementProps): JSX.Element => {
           ? 'flex-row'
           : 'flex-col'
       }`}
+      onClick={() =>
+        props.setElementOpened(
+          props.text === props.elementOpened ? null : props.text
+        )
+      }
     >
       <strong
         className={`absolute ${
           props.inverted && props.inRow
-            ? 'right-[-80px]'
+            ? 'right-[-110px]'
             : props.inverted
             ? 'bottom-[-50px]'
             : props.inRow
-            ? 'left-[-120px]'
+            ? 'left-[-160px]'
             : 'top-[-50px]'
-        } text-2xl`}
+        } ${
+          props.text === props.elementOpened ? 'text-4xl' : 'text-2xl'
+        } transition-all`}
       >
         {props.text}
       </strong>
-      <div className="bg-white-100/5 w-14 h-14 rounded-full flex items-center justify-center">
+      <div
+        className={`${
+          props.text === props.elementOpened
+            ? 'bg-white-100 w-20 h-20'
+            : 'bg-white-100/5 w-14 h-14'
+        }  rounded-full flex items-center justify-center transition-all`}
+      >
         <div
-          className={`bg-${props.color}-100 w-10 h-10 rounded-full flex items-center justify-center`}
+          className={`${props.color} ${
+            props.text === props.elementOpened
+              ? 'w-[60px] h-[60px]'
+              : 'w-10 h-10'
+          } rounded-full flex items-center justify-center transition-all`}
         >
-          <div className="bg-white-100/30 w-4 h-4 rounded-full" />
+          <div
+            className={`bg-white-100/30 ${
+              props.text === props.elementOpened ? 'w-7 h-7' : 'w-4 h-4'
+            } rounded-full transition-all`}
+          />
         </div>
       </div>
     </div>
--- src/components/folds/Flywheel.tsx
+++ src/components/folds/Flywheel.tsx
@@ -1,10 +1,23 @@
 import Image from 'next/image'
+import { useState } from 'react'
 
 import { BackgroundFlywheel } from '../backgrounds/BackgroundFlywheel'
+import { BackgroundFundraise } from '../backgrounds/BackgroundFundraise'
 import { BackgroundMain } from '../backgrounds/BackgroundMain'
+import { BackgroundSocial } from '../backgrounds/BackgroundSocial'
+import { BackgroundStart } from '../backgrounds/BackgroundStart'
+import { BackgroundVentures } from '../backgrounds/BackgroundVentures'
 import { FlywheelElement } from '../FlywheelElement'
+import { FundraiseImage } from '../images/FundraiseImage'
+import { SocialImage } from '../images/SocialImage'
+import { StartImage } from '../images/StartImage'
+import { VenturesImage } from '../images/VenturesImage'
+
+type Element = null | 'Social' | 'Start' | 'Fundraise' | 'Ventures'
 
 export const Flywheel = (): JSX.Element => {
+  const [elementOpened, setElementOpened] = useState<Element>(null)
+
   return (
     <div className="relative flex flex-col justify-center items-center h-screen">
       <div className="flex flex-col items-center gap-24 z-50">
@@ -24,17 +37,86 @@ export const Flywheel = (): JSX.Element => {
             />
           </div>
           <div className="flex flex-col justify-between absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 h-[500px] z-20">
-            <FlywheelElement text="Social" color="blue" />
-            <FlywheelElement text="Fundraise" color="green" inverted />
+            <FlywheelElement
+              text="Social"
+              color="bg-blue-100"
+              elementOpened={elementOpened}
+              setElementOpened={setElementOpened}
+            />
+            <FlywheelElement
+              text="Fundraise"
+              color="bg-green-100"
+              inverted
+              elementOpened={elementOpened}
+              setElementOpened={setElementOpened}
+            />
           </div>
           <div className="flex justify-between absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-[500px] z-20">
-            <FlywheelElement text="Ventures" color="gray" inRow />
-            <FlywheelElement text="Start" color="pink" inRow inverted />
+            <FlywheelElement
+              text="Ventures"
+              color="bg-gray-100"
+              inRow
+              elementOpened={elementOpened}
+              setElementOpened={setElementOpened}
+            />
+            <FlywheelElement
+              text="Start"
+              color="bg-pink-100"
+              inRow
+              inverted
+              elementOpened={elementOpened}
+              setElementOpened={setElementOpened}
+            />
           </div>
         </div>
       </div>
-      <BackgroundFlywheel />
-      <BackgroundMain dark />
+      {elementOpened === null && (
+        <>
+          <BackgroundFlywheel />
+          <BackgroundMain dark />
+        </>
+      )}
+      {elementOpened === 'Social' && (
+        <>
+          <p className="absolute top-24 left-12 max-w-[270px]">
+            Cobogo <strong>Social</strong> lets Creators present their business,
+            metrics and services to investors, brands, and their own audience.
+          </p>
+          <SocialImage />
+          <BackgroundSocial />
+        </>
+      )}
+      {elementOpened === 'Start' && (
+        <>
+          <p className="absolute top-24 left-12 max-w-[270px]">
+            Cobogo <strong>Start</strong> is the simplest way for Creators to
+            launch and run a US business without paperwork or legal headaches.
+          </p>
+          <StartImage />
+          <BackgroundStart />
+        </>
+      )}
+      {elementOpened === 'Fundraise' && (
+        <>
+          <p className="absolute top-24 left-12 max-w-[270px]">
+            Cobogo <strong>Fundraise</strong> allows Creators to tokenize equity
+            of their company and raise funds continuously using Rolling Safe.
+          </p>
+          <FundraiseImage />
+          <BackgroundFundraise />
+        </>
+      )}
+      {elementOpened === 'Ventures' && (
+        <>
+          <p className="absolute top-24 left-12 max-w-[270px]">
+            Cobogo <strong>Ventures</strong> is a seed/early-stage investor for
+            prominent Creators with a proprietary algorithm to estimate
+            Creators’ valuation.
+          </p>
+          <VenturesImage />
+          <BackgroundVentures />
+        </>
+      )}
     </div>
   )
 }
