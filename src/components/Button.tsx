66e7c5aa92bf4942fc3961a0b8ef3f8b0e8ad4f2
Thiago Machado <contatocostathiago@gmail.com>
2022-10-14 09:26:00 -0300

    fix: padding on Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps {
 export const Button = (props: ButtonProps): JSX.Element => {
   return (
     <div className="bg-gradient-to-r from-blue-100 via-green-100 to-pink-200 p-[2px]">
-      <button className="bg-blue-200 text-xl px-3 py-2 font-bold">
+      <button className="bg-blue-200 text-xl px-5 py-1 md:py-2 font-bold">
         {props.text}
       </button>
     </div>
