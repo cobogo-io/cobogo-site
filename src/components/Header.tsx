2f81726e010282d988e338775b6c429d1cbb0108
Thiago Machado <contatocostathiago@gmail.com>
2023-03-28 12:03:15 -0300

    feat: project setup and creates "Header" component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,186 +1,74 @@
-import { Menu } from '@headlessui/react'
-import { useTranslation } from 'next-i18next'
 import Image from 'next/image'
+
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-import { DropdownIcon } from './icons/DropdownIcon'
-
-export const Header = () => {
-  const { t } = useTranslation('common')
-
-  const { locale } = useRouter()
+export default function Header() {
+  const { asPath } = useRouter()
 
   return (
-    <div className="fixed px-12 py-9 hidden md:flex w-full gap-14 z-50 bg-header-gradient">
-      <Image
-        src="/images/cobogo-logo.svg"
-        width={120}
-        height={27}
-        alt="cobogo logo"
-      />
+    <div
+      className={`w-full flex items-center justify-between h-[76px] gap-2 lg:gap-0 px-7 lg:px-36 z-30 fixed bg-gradient-to-b from-black to-black/0`}
+    >
+      <div className="flex items-center gap-5 font-orbitron">
+        <Link href="/" className="flex">
+          <Image src="/images/logo.png" width={120} height={27} alt="Logo" />
+        </Link>
 
-      <div className="flex items-center justify-between w-full">
-        <div className="flex gap-7 relative items-center">
-          <Menu>
-            <Menu.Button className="font-bold">
-              <p className="flex items-center gap-2">
-                {t('Products')} <DropdownIcon />
-              </p>
-            </Menu.Button>
+        <div className="hidden lg:flex items-center gap-10 h-[50px] px-8">
+          {asPath === '/community' && (
+            <div className="w-[150px] h-[5px] bg-yellow-community fixed top-0 left-[315px] shadow-[0_10px_40px_0px_rgba(239,231,80,1)]" />
+          )}
+          {asPath === '/launchpad' && (
+            <div className="w-[155px] h-[5px] bg-pink-launchpad fixed top-0 left-[640px] shadow-[0_10px_40px_0px_rgba(245,174,255,1)]" />
+          )}
 
-            <Menu.Items className="bg-gradient-to-r from-blue-100 via-pink-100 to-gray-100 flex absolute p-1 top-[-16px] left-[-24px] font-bold">
-              <div className="flex flex-col items-start bg-blue-200 p-5 gap-1">
-                <Menu.Button className="mb-2">
-                  <p className="flex items-center gap-2 ">
-                    {t('Products')} <DropdownIcon />
-                  </p>
-                </Menu.Button>
-
-                <Menu.Item>
-                  <a href="#social">Social</a>
-                </Menu.Item>
-
-                <Menu.Item>
-                  <a href="#launchpad">Launchpad</a>
-                </Menu.Item>
-
-                <Menu.Item>
-                  <a href="#dao">DAO</a>
-                </Menu.Item>
-              </div>
-            </Menu.Items>
-          </Menu>
-
-          <Menu>
-            <Menu.Button className="font-bold">
-              <p className="flex items-center gap-2">
-                {t('Community')} <DropdownIcon />
-              </p>
-            </Menu.Button>
-
-            <Menu.Items
-              className={`bg-gradient-to-r from-blue-100 via-pink-100 to-gray-100 flex absolute p-1 ${
-                locale === 'pt'
-                  ? 'top-[-16px] left-[86px]'
-                  : 'top-[-16px] left-[84.5px]'
-              } font-bold`}
-            >
-              <div className="flex flex-col items-start bg-blue-200 p-5 gap-1">
-                <Menu.Button className="mb-2">
-                  <p className="flex items-center gap-2">
-                    {t('Community')} <DropdownIcon />
-                  </p>
-                </Menu.Button>
-
-                {/* <Menu.Item>
-                  <a
-                    target="_blank"
-                    href="https://l.cobogo.io/telegram/"
-                    rel="noreferrer"
-                  >
-                    Telegram
-                  </a>
-                </Menu.Item> */}
-
-                <Menu.Item>
-                  <a
-                    target="_blank"
-                    href={
-                      locale === 'pt'
-                        ? 'https://cobogobr.medium.com'
-                        : 'https://l.cobogo.io/medium'
-                    }
-                    rel="noreferrer"
-                  >
-                    Medium
-                  </a>
-                </Menu.Item>
-
-                <Menu.Item>
-                  <a
-                    target="_blank"
-                    href="https://l.cobogo.io/twitter/"
-                    rel="noreferrer"
-                  >
-                    Twitter
-                  </a>
-                </Menu.Item>
-
-                <Menu.Item>
-                  <a
-                    target="_blank"
-                    href="https://l.cobogo.io/github/"
-                    rel="noreferrer"
-                  >
-                    GitHub
-                  </a>
-                </Menu.Item>
-                {/* <Menu.Item>
-                  <a
-                    target="_blank"
-                    href="https://discord.gg/rKVJzyNADx"
-                    rel="noreferrer"
-                  >
-                    Discord
-                  </a>
-                </Menu.Item> */}
-              </div>
-            </Menu.Items>
-          </Menu>
-
-          <a
-            target="_blank"
-            href="https://l.cobogo.io/docs/"
-            className="flex font-bold"
-            rel="noreferrer"
+          <Link
+            href="/community"
+            className={`flex items-center gap-3 font-bold ${
+              asPath === '/launchpad' && 'brightness-75 saturate-0'
+            }`}
           >
-            Docs
-          </a>
+            <Image
+              src="/images/cobogo-community-icon.svg"
+              width={35}
+              height={35}
+              alt="Cobogo Community icon"
+            />
+            Community
+          </Link>
 
-          <a
-            target="_blank"
-            href={
-              locale === 'pt'
-                ? 'https://chat.whatsapp.com/LPPlb8bWgw38p9uZjrvlt0'
-                : 'https://discord.gg/4UsDWrfrpW'
-            }
-            className="bg-gradient-to-r from-blue-100 via-pink-100 to-gray-100 px-5 py-1 md:py-2 font-bold rounded-sm shadow-pink-100"
-            rel="noreferrer"
+          <Link
+            href="https://cobogo.social/"
+            className={`flex items-center gap-3 font-bold ${
+              (asPath === '/launchpad' || asPath === '/community') &&
+              'brightness-75 saturate-0'
+            }`}
           >
-            {t('Join our community')}
-          </a>
+            <Image
+              src="/images/cobogo-social-icon.svg"
+              width={35}
+              height={35}
+              alt="Cobogo Social icon"
+            />
+            Social
+          </Link>
+
+          <Link
+            href="/launchpad"
+            className={`flex items-center gap-3 font-bold ${
+              asPath === '/community' && 'brightness-75 saturate-0'
+            }`}
+          >
+            <Image
+              src="/images/cobogo-launchpad-icon.svg"
+              width={35}
+              height={35}
+              alt="Cobogo Launchpad icon"
+            />
+            Launchpad
+          </Link>
         </div>
-
-        <Menu>
-          <Menu.Button className="font-bold">
-            <p className="flex items-center gap-2">
-              {`${locale === 'pt' ? 'Portuguese' : 'English'}`} <DropdownIcon />
-            </p>
-          </Menu.Button>
-          <Menu.Items
-            className={`bg-gradient-to-r from-blue-100 via-pink-100 to-gray-100 flex absolute p-1 ${
-              locale === 'pt'
-                ? 'top-[20px] right-[24px]'
-                : 'top-[20px] right-[10px]'
-            } font-bold`}
-          >
-            <div className="flex flex-col items-start bg-blue-200 p-5 gap-1">
-              <Menu.Button className="mb-2">
-                <p className="flex items-center gap-2">
-                  {`${locale === 'pt' ? 'Portuguese' : 'English'}`}{' '}
-                  <DropdownIcon />
-                </p>
-              </Menu.Button>
-              <Menu.Item>
-                {locale !== 'en' ? (
-                  <a href="/en">English</a>
-                ) : (
-                  <a href="/pt">Portuguese</a>
-                )}
-              </Menu.Item>
-            </div>
-          </Menu.Items>
-        </Menu>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
+    './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'header-gradient':
-          'linear-gradient(180deg, rgba(0,0,0,1) 50%, rgba(0,212,255,0) 100%)',
+      fontFamily: {
+        'proxima-nova': ['Proxima Nova', ...defaultTheme.fontFamily.sans],
+        orbitron: ['Orbitron', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        white: {
-          100: '#FFFFFF',
-        },
-        black: {
-          900: '#000000',
-        },
-        blue: {
-          100: '#00B9E9',
-          200: '#16212E',
-        },
-        green: {
-          100: '#67A747',
-        },
-        pink: {
-          100: '#BA68C8',
-        },
-        gray: {
-          100: '#5C8295',
-        },
+        background: '#121212',
+        'yellow-community': '#EFE750',
+        'pink-launchpad': '#F5AEFF',
       },
     },
   },
   plugins: [],
 }
+
