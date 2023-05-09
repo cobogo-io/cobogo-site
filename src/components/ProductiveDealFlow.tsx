24249f25506471879c630c4344d0c1a66d77f973
Thiago Machado <contatocostathiago@gmail.com>
2023-05-09 14:44:11 -0300

    updates colors logic on "ProductiveDealFlow" component

--- src/components/ProductiveDealFlow.tsx
+++ src/components/ProductiveDealFlow.tsx
@@ -1,4 +1,3 @@
-import { Variants, motion } from 'framer-motion'
 import Image from 'next/image'
 import { Dispatch, SetStateAction } from 'react'
 
@@ -8,42 +7,6 @@ interface ProductiveDealFlowProps {
 }
 
 export default function ProductiveDealFlow(props: ProductiveDealFlowProps) {
-  const communityVariants: Variants = {
-    initial: {
-      backgroundColor: '#FFFFFF1A',
-    },
-    selected: {
-      backgroundColor: '#EFE750',
-    },
-    exit: {
-      backgroundColor: '#FFFFFF1A',
-    },
-  }
-
-  const socialVariants: Variants = {
-    initial: {
-      backgroundColor: '#FFFFFF1A',
-    },
-    selected: {
-      backgroundColor: '#00B9E9',
-    },
-    exit: {
-      backgroundColor: '#FFFFFF1A',
-    },
-  }
-
-  const launchpadVariants: Variants = {
-    initial: {
-      backgroundColor: '#FFFFFF1A',
-    },
-    selected: {
-      backgroundColor: '#F5AEFF',
-    },
-    exit: {
-      backgroundColor: '#FFFFFF1A',
-    },
-  }
-
   return (
     <div className="scale-50 lg:scale-100 absolute top-8 lg:top-16 left-1/2 -translate-x-1/2 z-10">
       <div className="absolute top-0 right-0">
@@ -56,31 +19,18 @@ export default function ProductiveDealFlow(props: ProductiveDealFlowProps) {
       </div>
 
       <div className="h-[220px] w-[260px] flex items-center justify-between clip-path-polygon-[0_0,_100%_45%,_100%_55%,_0_100%] relative">
-        <motion.button
-          animate={
-            props.selectedProduct === 'community' ? 'selected' : 'initial'
-          }
-          variants={communityVariants}
-          exit="exit"
-          className="w-[130px] h-full rounded-none"
+        <button
+          className="w-[130px] h-full rounded-none bg-white/10 focus:bg-yellow-community transition-colors"
           onClick={() => props.setSelectedProduct('community')}
         />
 
-        <motion.button
-          animate={props.selectedProduct === 'social' ? 'selected' : 'initial'}
-          variants={socialVariants}
-          exit="exit"
-          className="w-[60px] h-full rounded-none"
+        <button
+          className="w-[60px] h-full rounded-none bg-white/10 focus:bg-blue-social transition-colors"
           onClick={() => props.setSelectedProduct('social')}
         />
 
-        <motion.button
-          animate={
-            props.selectedProduct === 'launchpad' ? 'selected' : 'initial'
-          }
-          variants={launchpadVariants}
-          exit="exit"
-          className="w-[70px] h-full rounded-none"
+        <button
+          className="w-[70px] h-full rounded-none bg-white/10 focus:bg-pink-launchpad transition-colors"
           onClick={() => props.setSelectedProduct('launchpad')}
         />
       </div>
